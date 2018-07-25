f0baca3d61b2480aedf35cdb97ecfa9659b060b6
851518253@qq.com <851518253@qq,com>
2018-07-25 23:34:53 +0800

    23:34

--- hashlib.py
+++ hashlib.py
@@ -1,13 +1,16 @@
 import hashlib
 
 obj = hashlib.md5("sfdsg".encode("utf8"))   #加盐加密
+obj3 = hashlib.sha256("sfdsg".encode("utf8"))   #加盐加密
+obj1 = hashlib.md5("sfdsg".encode("utf8"))
 
 obj.update("hello".encode("utf8"))
-
+obj2.update("hello".encode("utf8"))
+obj3.update("hello".encode("utf8"))
 print(obj.hexdigest())
+print(obj1.hexdigest())
+print(obj3.hexdigest())
 
-obj = hashlib.sha256("sfdsg".encode("utf8"))   #加盐加密
 
-obj.update("hello".encode("utf8"))
 
-print(obj.hexdigest())
+
