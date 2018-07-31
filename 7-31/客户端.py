c660f222c2a80a57918c2ecc735f636d0fea3f62
851518253@qq.com <851518253@qq,com>
2018-07-31 16:16:52 +0800

    2018/7/31 16.16

--- 7-31/客户端.py
+++ 7-31/客户端.py
@@ -1,11 +1,16 @@
 import socket
 
+ip_port = ('172.24.36.5',8000)
+buffer_size = 1021
+
 phone = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-phone.connect(('127.0.0.1',8000)) #拨通电话
+# phone.connect(('127.0.0.1',8000)) #拨通电话    本机
+# phone.connect(('172.24.36.29',8000)) #拨通电话
+phone.connect(ip_port) #拨通电话
 
 phone.send('Rfdsd'.encode('utf8'))
 
-data = phone.recv(2048)
+data = phone.recv(buffer_size)
 
 print('收到服务器端发送的消息为',data)
--- 7-31/服务端.py
+++ 7-31/服务端.py
@@ -1,15 +1,20 @@
 import socket
 
+ip_port = ('172.24.36.5',8000)
+back_log = 5
+buffer_size = 1021
+
 phone = socket.socket(socket.AF_INET,socket.SOCK_STREAM)#网络通信基于TCP  流式链接
 # AF_INET代表基于网络类型的套接字   SOCK_STREAM代表TCP协议
 
-phone.bind(('127.0.0.1',8000))  #IP地址+端口
+# phone.bind(('127.0.0.1',8000))  #IP地址+端口
+phone.bind(ip_port)  #IP地址+端口
 
-phone.listen(5)  #表示最多有5个等待
+phone.listen(back_log)  #表示最多有5个等待
 print('正在等电话')
 conn,addr =phone.accept()  #等电话    链接，手机号（地址）
 
-msg = conn.recv(1024) #收消息
+msg = conn.recv(buffer_size) #收消息
 print('客户端发来的消息是:',msg)
 
 conn.send(msg.upper())#发消息
