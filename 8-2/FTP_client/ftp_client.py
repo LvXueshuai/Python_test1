3c223c40f4f51dd0edaa9ed48242468537ddcec7
851518253@qq.com <851518253@qq,com>
2018-08-03 20:57:43 +0800

    2018/8/3 20.57

--- 8-2/FTP_client/ftp_client.py
+++ 8-2/FTP_client/ftp_client.py
@@ -2,6 +2,7 @@ import optparse
 import socket
 import configparser    #文件配置
 import json
+import os,sys
 
 
 STATUS_CODE = {
@@ -11,6 +12,7 @@ STATUS_CODE = {
 
 
 class ClientHandler():
+
     def __init__(self):
         self.op = optparse.OptionParser()
 
@@ -23,6 +25,8 @@ class ClientHandler():
 
         self.verify_args(self.options,self.args)
         self.make_connection()
+        self.mainPath = os.path.dirname(os.path.abspath(__file__))
+        self.last = 0
 
     def verify_args(self,options,args):
 
@@ -40,11 +44,109 @@ class ClientHandler():
         self.sock.connect((self.options.server,int(self.options.port)))
 
     def ineractive(self):
-        self.authenticate()
+        print('Begin To Interactive......')
 
+        if self.authenticate():
+
+            cmd_info = input('【%s】'%self.current_dir).strip()
+            cmd_list = cmd_info.split()
+
+            if hasattr(self,cmd_list[0]):   #反射
+                func = getattr(self,cmd_list[0])
+                func(*cmd_list)
+
+    def put(self,*cmd_list):
+
+        action,local_path,target_path = cmd_list
+        local_path = os.path.join(self.mainPath,local_path)
+
+        file_name = os.path.basename(local_path)
+        file_size = os.stat(local_path).st_size
+
+        data = {
+            'action':'put',
+            'file_name':file_name,
+            'file_size':file_size,
+            'target_path':target_path
+        }
+
+        self.sock.send(json.dumps(data).encode('utf8'))
+
+        is_exist = self.sock.recv(1024).decode('utf8')
+
+        has_sent = 0
+        if is_exist == '800':
+            # 文件不完整
+            choice = input('文件已存在，但不完整，是否继续上传？[Y/N]').strip()
+            if choice.upper() == 'Y':
+                self.sock.send('Y'.encode('utf8'))
+                continue_position = self.response(1024)
+                has_sent += int(continue_position)
+
+            else:
+                self.sock.send('N'.encode('utf8'))
+
+        elif is_exist == '801':
+            #文件完全存在
+            print('该文件已存在！')
+            return
+
+        else:
+            pass
+
+        f = open(local_path,'rb')
+        f.seek(has_sent)
+        while has_sent < file_size:
+            data = f.read(1024)
+            self.sock.sendall(data)
+            has_sent += len(data)
+            self.show_progress(has_sent,file_size)
+
+        f.close()
+        print('上传成功')
+
+    def show_progress(self,has,total):   #进度条
+        rate = float(has)/float(total)
+        rate_number = int(rate * 100)
+        # if self.last != rate_number:
+        #     sys.stdout.write('%s%% %s\r' %(rate_number,'#'*rate_number))
+        # self.last = rate_number
+        sys.stdout.write('%s%% %s\r' % (rate_number, '#' * rate_number))
+
+    def ls(self,*cmd_list):
+        data = {
+            'action':'ls',
+        }
+        self.sock.sendall(json.dumps(data).encode('utf8'))
+
+        data = self.sock.recv(1024).decode('utf8')
+        print(data)
+
+    def cd(self, *cmd_list):
+        data = {
+            'action': 'cd',
+            'dirname':cmd_list[1],
+
+        }
+        self.sock.sendall(json.dumps(data).encode('utf8'))
+
+        data = self.sock.recv(1024).decode('utf8')
+        print(os.path.basename(data))
+        self.current_dir = os.path.basename(data)  #显示为当前文件夹名称
+
+
+    def mkdir(self, *cmd_list):
+        data = {
+            'action':'mkdir',
+            'dirname':cmd_list[1],
+        }
+        self.sock.sendall(json.dumps(data).encode('utf8'))
+        data = self.sock.recv(1024).decode('utf8')
+        print(data)
 
 
     def authenticate(self):
+
         if self.options.username is None or self.options.password is None:
             username = input('username:')
             password = input('password:')
@@ -57,6 +159,7 @@ class ClientHandler():
         data = json.loads(data)
         return data
 
+
     def get_auth_result(self,user,pwd):
 
         data = {
@@ -67,11 +170,17 @@ class ClientHandler():
 
         self.sock.send(json.dumps(data).encode('utf8'))
 
+
+
+
+
         response = self.response()
         print('RESPONSE:',response['status_code'])
         if response['status_code'] ==254:
             self.user = user
+            self.current_dir = user
             print(STATUS_CODE[254])
+            return True
         else:
             print(STATUS_CODE[response['status_code']])
 
